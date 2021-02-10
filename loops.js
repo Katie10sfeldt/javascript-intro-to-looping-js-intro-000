e682816da903d7b4f4a91531a24f3a30969ae2d1
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-10 16:13:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 //For loop goes Here
 
 
-//While loop
+//While loop - takes num as a parameter and counts down from given number
+// When done, returns 'done'
+// -- is working
 
 function whileLoop(num) {
   while(num > 0) {
@@ -9,4 +11,17 @@ function whileLoop(num) {
     num--;
   }
   return 'done';
+}
+
+//Do While Loops
+
+function doWhileLoop(int) {
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+  }
+
+  do {
+    console.log('I run once regardless.');
+  } while (incrementVariable() < num);
 }
